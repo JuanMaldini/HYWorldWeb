2297cc5ce5ded39ba38900794862fcc6984cdbb1
Juan Maldini <juan.maldini@outlook.com>
2026-06-03 17:50:21 -0300

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/project': 'http://localhost:5000',
+      '/api': 'http://localhost:5000',
+      '/projects': 'http://localhost:5000',
+      '/static': 'http://localhost:5000',
+    }
+  }
 })
